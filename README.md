abbcb5057db6fa63033b5b2b50a2f3cb750ee6e4
nyk.yeon <nyk.yeon@gmail.com>
2024-02-16 15:36:07 +0900

    핀 순서 수정 및 안내 수정

--- README.md
+++ README.md
@@ -11,14 +11,10 @@ https://www.eleparts.co.kr/EPXMVGDB
 https://github.com/eleparts/iotkit  
 
 
-## blynk_python_GPIO.py  
-
-https://github.com/vshymanskyy/blynk-library-python
-
-위 라이브러리를 설치 후 파일을 실행해 주면 됩니다.  
-``python blynk_python_GPIO.py``  
-
-라즈베리파이에서 라이브러리 설치 시 아래 명령으로 실행해야 합니다.  
+## 사용 전 필수 라이브러리  
+  
+[**blynk - Python 라이브러리**](https://github.com/vshymanskyy/blynk-library-python)를 설치 후 파일을 실행해 주면 됩니다.  
+라즈베리파이에서 아래 명령을 차례대로 입력해 주시면 라이브러리를 간단하게 설치할 수 있습니다.  
 
 ```bash
 # 복제된 lynk-library-python 라이브러리
@@ -26,58 +22,58 @@ git clone https://github.com/eleparts/blynk-library-python
 cd blynk-library-python
 sudo python setup.py install
 ```
+  
+> - 라즈베리파이5를 사용한다면 추가로 pi5용 lgpio 라이브러리를 설치해 줍니다.  
+> ```bash
+> # 반드시 라즈베리파이 5 사용시에만 설치
+> pip install --break-system-packages rpi-lgpio
+> ```
+  
+## blynk_python_GPIO.py  
 
-만약 라즈베리파이5를 사용한다면 아래 명령어로 pi5 GPIO 라이브러리를 설치해 주어야 합니다.
+  예제는 아래 명령어로 실행할 수 있습니다.  
+  
+``python blynk_python_GPIO.py``  
 
-```bash
-# 반드시 라즈베리파이 5 사용시에만 설치
-pip install --break-system-packages rpi-lgpio
-```
+## Blynk 위젯 배치  
 
-## blynk_GPIO.fzz
+- PI - GPIO 디바이스 설정 
+
+|가상핀|가상핀 설정|위젯|모드|
+|:----:|:-:|:-:| :-:|
+|V0 | integer 0/1 | Button | MODE : SWITCH | 
+|V1 | integer 0/1 | Button | MODE : SWITCH |  
+|V2 | string| Value Display | | 
+|V4 | integer 0/255|LED | | 
+------------------------------------------  
+
+## blynk_GPIO.fzz  
 fritzing 확장자 파일입니다.  
 fritzing이 설치되어 있다면 열어보실 수 있습니다.  
 http://fritzing.org/home/  
 
 
-## blynk_GPIO.png
+## blynk_GPIO.png  
 회로 구성 이미지 파일입니다.  
 ![blynk_GPIO](https://raw.githubusercontent.com/eleparts/blynk_python_GPIO/master/blynk_GPIO.png)  
 > LED 저항 : 100 ~ 220옴  
 > SW 저항 : 1K ~ 10K옴 (풀다운 저항)  
 
-예제코드 동작 전에 해당 회로를 똑같이 구성해 주셔야 합니다.  
+예제코드 동작 전에 해당 회로를 동일하게 구성해 주셔야 합니다.  
+
+## 소스코드 폴더  
+
+**소스 코드**가 버전별로  **.py** 파일로 저장되어 있습니다.  
 
 
-
-## 소스코드 폴더
-
-**소스 코드**가 버전별로  **.py** 파일로 저장되어 있습니다.
-
-
-### 버전별 변경 내역
-
-- blynk_python_GPIO_v1.00  
-기본 예제 코드입니다.  
-
-> 2개의 VIRTUAL_WRITE  
-> 1개의 VIRTUAL_READ  
-> my_user_task 작업으로 구성되어 있습니다.  
-  
-- blynk_python_GPIO_v1.01  
-VIRTUAL_READ 에서 **호출될 때마다 GPIO값을 터미널로 출력**하는 부분을 **변경되는 시점에서 1회 출력**으로 수정하였습니다.  
-
-- blynk_python_GPIO_v1.10  
-blynk.set_user_task 함수가 삭제되고 타이머 라이브러리가 추가되었습니다.  
-타이머 라이브러리를 다운로드 하기 위하여 start.sh 스크립트가 추가되었습니다.  
-
-- blynk_python_GPIO_v2.0.0  
-blynk가 공식 python 예제를 지원하게 되면서 기존 베타 버전 라이브러리와 다른 라이브러리를 사용하게 되었습니다.  
-이에 따라 해당 공식 라이브러리를 사용하도록 수정되었습니다.  
+### 버전별 변경 내역  
 
 - blynk_python_GPIO_v3.0.0  
 blynk 2.0 대응 라이브러리 사용을 위해 업데이트 된 기존 저장소를 이용합니다.  
 pip로 설치 시 구버전이 설치되어(0.2.6) 해당 저장소를 직접 받아 설치해야 합니다.(1.0.0)  
+
+- blynk_python_GPIO_v3.1.0  
+Vpin 순서 변경, 안내 추가  
   
 ## old_version  
 
--- blynk_python_GPIO.py
+++ blynk_python_GPIO.py
@@ -1,12 +1,14 @@
 """
-* blynk GPIO 제어 프로그램 blynk 2.0 대응
-* 수정 : 2024. 01. 31
+* blynk GPIO 제어 프로그램
+* 수정 : 2024. 02. 16
 * 제작 : eleparts 부설연구소
-* SW ver. 3.0.0
+* SW ver. 3.1.0
 
-3.0.0 변경사항
-> blynk 2.0 대응 업데이트
-blynk-library-python 최신버전 v1.0.0 을 직접 받아 설치해야 합니다.
+> Vpin
+V0 - integer 0/1    : Button / MODE : SWITCH
+V1 - integer 0/1    : Button / MODE : SWITCH
+V2 - string         : Value Display
+V4 - integer 0/255  : LED
 
 기반 코드 및 필수 라이브러리 - blynk / python (Blynk Python Library V1.0.0)
 https://github.com/vshymanskyy/blynk-library-python
@@ -17,7 +19,7 @@ import BlynkLib
 import RPi.GPIO as GPIO 
 from BlynkTimer import BlynkTimer
 
-#이메일로 받은 토큰을 여기에 추가
+# 사용자 토큰을 여기에 추가
 BLYNK_AUTH = 'YourAuthToken'
 
 # Initialize Blynk
@@ -36,8 +38,8 @@ GPIO.setup(16,GPIO.IN, GPIO.PUD_DOWN)
 # 부팅 시 서버 데이터 받아와 동기화 - LED 상태 동기화
 @blynk.on("connected")
 def blynk_connected():
-    print("Updating V1,V2 values from the server...")
-    blynk.sync_virtual(1,2)
+    print("Updating V0,V1 values from the server...")
+    blynk.sync_virtual(0,1)
 
 # Virtual 핀 변경사항 터미널에 출력 - 디버그
 @blynk.on("V*")
@@ -47,17 +49,17 @@ def blynk_handle_vpins(pin, value):
 
 
 # Register Virtual Pins
-# blynk앱에서 버튼 누를경우 동작 - write (Virtual Pins 1)
-@blynk.on("V1")
-def handler_V1(value):
+# blynk앱에서 버튼 누를경우 동작 - write (Virtual Pins 0)
+@blynk.on("V0")
+def handler_V0(value):
     if(value == ['1']):
         GPIO.output(21, 1)
     else:
         GPIO.output(21, 0)
 
-# blynk앱에서 버튼 누를경우 동작 - write (Virtual Pins 2)
-@blynk.on("V2")
-def handler_V2(value):
+# blynk앱에서 버튼 누를경우 동작 - write (Virtual Pins 1)
+@blynk.on("V1")
+def handler_V1(value):
     if(value == ['1']):
         GPIO.output(20, 1)
     else:
@@ -69,26 +71,26 @@ def handler_V2(value):
 def hello_world():
     print("Hello World!")
 
-# 타이머 사용해 주기적으로 GPIO 읽어 변경 시 blynk앱으로 전송 - (Virtual Pins 3)
-@blynk.on("V3")
-def handler_V3():
+# 타이머 사용해 주기적으로 GPIO 읽어 변경 시 blynk앱으로 전송 - (Virtual Pins 2)
+@blynk.on("V2")
+def handler_V2():
     SW = GPIO.input(16)
 
     try:  
-        if(SW != handler_V3.lastSW):
+        if(SW != handler_V2.lastSW):
             print("SW GPIO value : %d "% (SW))
 
             if(SW):
-                blynk.virtual_write(3, " ON")
-                handler_V3.lastSW = SW
+                blynk.virtual_write(2, " ON")
+                handler_V2.lastSW = SW
                 
             else:
-                blynk.virtual_write(3, "OFF")
-                handler_V3.lastSW = SW
+                blynk.virtual_write(2, "OFF")
+                handler_V2.lastSW = SW
 
     # 최초 1회 - 스위치 기존 value값 저장용 변수 선언
     except AttributeError:
-        handler_V3.lastSW = False
+        handler_V2.lastSW = False
 
 # 타이머 테스트용 함수 - LED ON/OFF (Virtual Pins 4)
 @blynk.on("V4")
@@ -113,7 +115,7 @@ def LED_task():
 # Add Timers
 # timer : 설정해 둔 시간마다 실행됨
 timer.set_timeout(1, hello_world) # 최초 1회, 1초후 실행
-timer.set_interval(1, handler_V3) # 1초마다 반복실행
+timer.set_interval(1, handler_V2) # 1초마다 반복실행
 timer.set_interval(3, LED_task) # 3초마다 반복실행
 
 
